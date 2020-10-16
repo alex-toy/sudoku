537e1e7efbbfce2ac5e0a715c2dbdb14248f0bac
Anael AEBY <anael.aeby@gmail.com>
2020-10-16 18:13:27 +0200

    maj 16/10/2020 v2

--- src/sudoku/AlgoSudoku.java
+++ src/sudoku/AlgoSudoku.java
@@ -1,28 +1,33 @@
-public final class algo_sudoku {
+package sudoku;
 
-	class affichegrillevide {}
+public final class AlgoSudoku {
 	
-	class recuperegrillechiffre {}
+	static int rows = 9;
+	static int columns = rows;
+	static int[][][] matrice;
+
+	public static void CreationMatrice() {
+		matrice = new int[rows][columns][9];
+		for (int r = 0 ; r < rows ; r++ ) {
+			for (int c = 0 ; c < columns ; c++) {
+				matrice[r][c] = new int[]{1,2,3,4,5,6,7,8,9};
+			}
+		}
+	}
 	
-	class creationmatrice {}
+	public static void main(final String[] args) {
+		TestAffichage();
+	}
 	
-	class remplissagecarre {}
-	
-	class remplissageligne {}
-	
-	class remplissagecolonne {}
-	
-	class afficheresultatsudoku {}
-	
-	class verificateursudoku {}
-	
-	
-	
-	class main {
-	
-		boolean termine = false;
-	
-		
-	
+	public static void TestAffichage() {
+		CreationMatrice();
+		matrice[2][0] = new int[] {4};
+		for (int r = 0 ; r < rows ; r++ ) {
+			if (r > 0) {System.out.println();}
+			for (int c = 0 ; c < columns ; c++) {
+				if (c > 0) {System.out.print(" | ");}
+				System.out.print(matrice[r][c].length);
+			}
+		}
 	}
 }
