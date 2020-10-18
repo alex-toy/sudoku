0963f773f2c19201419339af83998b4513474d1a
Alessio Rea <alexei.80@hotmail.fr>
2020-10-18 13:03:54 +0200

    progress

--- src/FormEvent.java
+++ src/FormEvent.java
@@ -4,12 +4,20 @@ public class FormEvent extends EventObject {
 	
 	private String name;
 	private String occupation;
+	private int ageCategory;
+	private String empCat;
+	private boolean usCitizen;
+	private String taxId;
 
-	public FormEvent(Object source, String name, String occupation) {
+	public FormEvent(Object source, String name, String occupation, int ageCat, String empCat, String taxId, boolean usCitizen) {
 		super(source);
 		
 		this.name = name;
 		this.occupation = occupation;
+		this.setAgeCategory(ageCat);
+		this.setEmpCat(empCat);
+		this.setTaxId(taxId);
+		this.setUsCitizen(usCitizen);
 	}
 
 	public String getName() {
@@ -27,5 +35,39 @@ public class FormEvent extends EventObject {
 	public void setOccupation(String occupation) {
 		this.occupation = occupation;
 	}
+
+	public int getAgeCategory() {
+		return ageCategory;
+	}
+
+	public void setAgeCategory(int ageCategory) {
+		this.ageCategory = ageCategory;
+	}
+
+	public String getEmpCat() {
+		return empCat;
+	}
+
+	public void setEmpCat(String empCat) {
+		this.empCat = empCat;
+	}
+
+	public boolean isUsCitizen() {
+		return usCitizen;
+	}
+
+	public void setUsCitizen(boolean usCitizen) {
+		this.usCitizen = usCitizen;
+	}
+
+	public String getTaxId() {
+		return taxId;
+	}
+
+	public void setTaxId(String taxId) {
+		this.taxId = taxId;
+	}
+	
+	
 
 }
--- src/FormPanel.java
+++ src/FormPanel.java
@@ -6,8 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.border.Border;
@@ -20,6 +25,11 @@ public class FormPanel extends JPanel {
 	private JTextField occupationField;
 	private JButton oKBtn;
 	private FormListener formListener;
+	private JList ageList;
+	private JComboBox empCombo;
+	private JCheckBox citizenCheck;
+	private JTextField taxField;
+	private JLabel taxLabel;
 	
 	
 	public FormPanel() {
@@ -32,13 +42,53 @@ public class FormPanel extends JPanel {
 		nameField = new JTextField(10);
 		occupationField = new JTextField(10);
 		
+		ageList = new JList();
+		DefaultListModel ageModel = new DefaultListModel();
+		ageModel.addElement(new AgeCategory(1, "under 18"));
+		ageModel.addElement(new AgeCategory(2, "18 to 65"));
+		ageModel.addElement(new AgeCategory(3, "65 over"));
+		ageList.setModel(ageModel);
+		ageList.setPreferredSize(new Dimension(110, 66));
+		ageList.setBorder(BorderFactory.createEtchedBorder());
+		ageList.setSelectedIndex(1);
+		
+		empCombo = new JComboBox();
+		DefaultComboBoxModel empModel = new DefaultComboBoxModel();
+		empModel.addElement("employed");
+		empModel.addElement("self_employed");
+		empModel.addElement("unemployed");
+		empCombo.setModel(empModel);
+		empCombo.setSelectedIndex(0);
+		empCombo.setEditable(true);
+		
+		citizenCheck = new JCheckBox();
+		citizenCheck.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				boolean is_ticked = citizenCheck.isSelected();
+				taxLabel.setEnabled(is_ticked);
+				taxField.setEnabled(is_ticked);
+			}
+		});
+		
+		taxField = new JTextField(10);
+		taxField.setEnabled(false);
+		
+		taxLabel = new JLabel("tax Id : ");
+		taxLabel.setEnabled(false);
+		
 		oKBtn = new JButton("OK");
 		oKBtn.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent arg0) {
 				String name = nameField.getText();
 				String occupation = occupationField.getText();
+				AgeCategory ageCat = (AgeCategory) ageList.getSelectedValue();
+				String empCat = (String)empCombo.getSelectedItem();
+				String taxId = taxField.getText();
+				boolean usCitizen = citizenCheck.isSelected();
 				
-				FormEvent ev = new FormEvent(this, name, occupation);
+				System.out.println(empCat);
+				
+				FormEvent ev = new FormEvent(this, name, occupation, ageCat.getId(), empCat, taxId, usCitizen);
 				
 				if(formListener != null) {
 					formListener.formEventOccured(ev);
@@ -51,17 +101,25 @@ public class FormPanel extends JPanel {
 		Border outerBorder = BorderFactory.createEmptyBorder(5, 5, 5, 5);
 		setBorder(BorderFactory.createCompoundBorder(outerBorder, innerBorder));
 		
+		layoutComponents();
+		
+	}
+	
+	
+	public void layoutComponents() {
+		
 		setLayout(new GridBagLayout());
 		
 		GridBagConstraints gc = new GridBagConstraints();
 		
 		////////First row///////////////////////////////////////
 		
+		gc.gridy = 0;
+		
 		gc.weightx = 1;
 		gc.weighty = 0.1;
 		
 		gc.gridx = 0;
-		gc.gridy = 0;
 		gc.fill = GridBagConstraints.NONE;
 		gc.anchor = GridBagConstraints.LINE_END;
 		gc.insets = new Insets(0, 0, 0, 5);
@@ -74,39 +132,132 @@ public class FormPanel extends JPanel {
 		add(nameField, gc);
 		
 		////////Second row///////////////////////////////////////
+
+		gc.gridy++;
 		
 		gc.weightx = 1;
 		gc.weighty = 0.1;
 		
 		gc.gridx = 0;
-		gc.gridy = 1;
 		gc.anchor = GridBagConstraints.LINE_END;
 		add(occupationLabel, gc);
 		
 		gc.gridx = 1;
-		gc.gridy = 1;
 		gc.insets = new Insets(0, 0, 0, 0);
 		gc.anchor = GridBagConstraints.LINE_START;
 		add(occupationField, gc);
 		
-		
 		////////third row///////////////////////////////////////
+		
+		gc.gridy++;
+		
+		gc.weightx = 1;
+		gc.weighty = 0.2;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("age : "), gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(ageList, gc);
+		
+		
+		////////fourth row///////////////////////////////////////
+		
+		gc.gridy++;
+		
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("Employment : "), gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(empCombo, gc);
+		
+		////////fourth row///////////////////////////////////////
+		
+		gc.gridy++;
+		
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(new JLabel("US citizen : "), gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(citizenCheck, gc);
+		
+		
+		////////fourth row///////////////////////////////////////
+		
+		gc.gridy++;
+		
+		gc.weightx = 1;
+		gc.weighty = 2.0;
+		
+		gc.gridx = 0;
+		gc.anchor = GridBagConstraints.FIRST_LINE_END;
+		gc.insets = new Insets(0, 0, 0, 5);
+		add(taxLabel, gc);
+		
+		gc.gridx = 1;
+		gc.anchor = GridBagConstraints.FIRST_LINE_START;
+		gc.insets = new Insets(0, 0, 0, 0);
+		add(taxField, gc);
+		
+		
+		////////fifth row///////////////////////////////////////
+		
+		gc.gridy++;
 		
 		gc.weightx = 1;
 		gc.weighty = 2.0;
 		
 		gc.gridx = 1;
-		gc.gridy = 2;
 		gc.anchor = GridBagConstraints.FIRST_LINE_START;
 		gc.insets = new Insets(0, 0, 0, 0);
 		add(oKBtn, gc);
 		
 	}
 	
+	
 	public void setFormListener(FormListener listener){
 		this.formListener = listener;
 	}
 
+}
+
+class AgeCategory {
+	
+	private int id;
+	private String text;
+	
+	public AgeCategory(int id, String text) {
+		this.id = id;
+		this.text = text;
+	}
+	
+	public String toString() {
+		return text;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	
 }
 
 
